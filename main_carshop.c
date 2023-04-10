24f0e7d553d07794700ecb81a5ea7e038c31ab1a
charumotiramani <78335843+charumotiramani@users.noreply.github.com>
2023-04-10 16:03:54 +0530

    Add files via upload

---
+++ main_carshop.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct car_shop
+
+{
+
+    char name[20];
+
+    int year;
+
+    float cost;
+
+    char category;
+
+};
+
+
+int main()
+
+{
+
+    struct car_shop x[4];
+
+
+    printf("Name: ");
+
+    scanf("%s",&x[0].name);
+
+    printf("Manufacturing Year: ");
+
+    scanf("%d",&x[1].year);
+
+    printf("Cost: ");
+
+    scanf("%f",&x[2].cost);
+
+    printf("Category(H/M/L): ");
+
+    scanf("%c",&x[3].category);
+
+        
+
+    printf("\nCar Name: %s \nManufacturing Year: %d \nCost: %f \nCategory: %c \n ",x[0].name,x[1].year,x[2].cost,x[3].category);
+
+
+    return 0;
+
+}
